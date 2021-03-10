811414198f6c8ce09c8a8d03a3b5ed9e302f6484
Nikita Derevskov <nikitaderevskov@gmail.com>
2021-03-11 00:45:47 +0300

    react-classes: add work with fetch, error handling, loading and etc

--- react-classes-todolist/src/containers/App.js
+++ react-classes-todolist/src/containers/App.js
@@ -9,21 +9,37 @@ class App extends Component {
             todos: [],
             completedTodos: [],
             hasError: false,
+            error: '',
             isPending: true
         }
     }
 
-    componentDidMount() {
+    fetchTodos() {
         fetch("https://jsonplaceholder.typicode.com/todos/")
+            .then(response => {if (!response.ok) {
+                throw new Error(`${response.statusText} ${response.status}`)
+            } else { return response }})
             .then(response => response.json())
             .then(data => this.setState({todos: data}))
+            .catch(error => this.setState({hasError: true, error}))
+            .finally(() => this.setState({isPending: false}))
+    }
+
+    componentDidMount() {
+        this.fetchTodos()
+    }
+
+    calcTodosPage() {
+        const {isPending, hasError, todos, completedTodos} = this.state
+        if (isPending) return 'Loading...'
+        else if (hasError) return this.state.error
+        else return <TodoList todos={todos} completedTodos={completedTodos}/>
     }
 
     render() {
         return (
             <section className="todos-page">
-                {` ${this.state.isPending}` }
-                <TodoList />
+                { this.calcTodosPage()}}
             </section>)
     }
 }
